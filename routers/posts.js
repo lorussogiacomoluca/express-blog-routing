f785d6fcbee9e695180d8d70a98f2187372c2dc7
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-17 13:40:43 +0200

    [UPD} Route created

---
+++ routers/posts.js
@@ -0,0 +1,34 @@
+//Express import
+const express = require('express')
+
+// Create a new router
+const router = express.Router()
+
+router.get('/', (req, res) =>{
+    res.send(`Lista dei post`)
+})
+
+router.get('/:id', (req,res) =>{
+    res.send(`Dettaglio del post: ${req.params.id}`)
+})
+
+//STORE
+router.post('', (req, res) =>{
+    res.send('Inserimento di un nuovo post')
+})
+
+//UPDATE
+router.put('/:id', (req, res)=>{
+    res.send(`Modifica totale del post: ${req.params.id}`)
+})
+
+//MODIFICA PARZIALE
+router.patch('/:id',(req,res)=>{
+    res.send(`Modifica parziale del post: ${req.params.id}`)
+})
+
+//DESTROY
+router.delete('/:id', (req,res)=>{
+    res.send(`Cancellazione del post: ${req.params.id}`)
+})
+
