3d1e216b9a8c40a70e2a309d7e8c8b582f7974a3
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-17 13:37:27 +0200

    [UPD] Import express and set get and listen

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+//Express import
+const express = require('express')
+//Create express object 
+const app = express()
+//Port
+const port = 3000
+
+// Shows "Il mio blog" when visiting "/"
+app.get('/', (req,res)=>{
+    res.send('Il mio blog')
+})
+
+// Starts the server and logs the port
+app.listen(port, ()=>{
+    console.log(`Listening on port: ${port}`)
+})
